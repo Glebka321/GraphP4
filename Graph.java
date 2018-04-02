7f06f552680b48eddb8ad5ee540cf2814828c45c
Glebka321 <gt.wildrocket@gmail.com>
2018-04-02 17:35:06 -0500

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,82 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Undirected and unweighted graph implementation
+ * 
+ * @param <E> type of a vertex
+ * 
+ * @author sapan (sapan@cs.wisc.edu)
+ * 
+ */
+public class Graph<E> implements GraphADT<E> {
+    
+    /**
+     * Instance variables and constructors
+     */
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public E addVertex(E vertex) {
+        return vertex;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public E removeVertex(E vertex) {
+        return vertex;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean addEdge(E vertex1, E vertex2) {
+        return false;
+        
+    }    
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean removeEdge(E vertex1, E vertex2) {
+        return false;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isAdjacent(E vertex1, E vertex2) {
+        return false;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterable<E> getNeighbors(E vertex) {
+        return null;
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterable<E> getAllVertices() {
+        return null;
+        
+    }
+
+}
