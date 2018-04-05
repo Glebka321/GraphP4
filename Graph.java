95eef77639360fc67f8f44c05a6a878d45192cf0
ErikUmhoe <98eriku@gmail.com>
2018-04-05 18:49:10 -0500

    First initial changes

--- Graph.java
+++ Graph.java
@@ -1,3 +1,6 @@
+package GraphP4;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,12 +18,23 @@ public class Graph<E> implements GraphADT<E> {
     /**
      * Instance variables and constructors
      */
+	
+	private ArrayList<E> vertices;
+	private boolean[][] edges;
+	
+	public Graph()
+	{
+		vertices = new ArrayList<E>();
+		edges = new boolean[20][20];
+	}
+	
 
     /**
      * {@inheritDoc}
      */
     @Override
     public E addVertex(E vertex) {
+        vertices.add(vertex);
         return vertex;
         
     }
@@ -29,8 +43,14 @@ public class Graph<E> implements GraphADT<E> {
      * {@inheritDoc}
      */
     @Override
+    //Test
     public E removeVertex(E vertex) {
-        return vertex;
+    	if(vertex == null)
+    		return null;
+		if(vertices.remove(vertex))
+			return vertex;
+		else
+			return null;
         
     }
 
@@ -39,6 +59,12 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean addEdge(E vertex1, E vertex2) {
+    	if(vertices.contains(vertex1) && vertices.contains(vertex2))
+    	{
+    		edges[vertices.indexOf(vertex1)][vertices.indexOf(vertex2)] = true;
+    		edges[vertices.indexOf(vertex2)][vertices.indexOf(vertex1)] = true;
+    		return true;
+    	}
         return false;
         
     }    
@@ -48,7 +74,12 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean removeEdge(E vertex1, E vertex2) {
-        return false;
+    	if(vertices.contains(vertex1) && vertices.contains(vertex2))
+    	{
+    		edges[vertices.indexOf(vertex1)][vertices.indexOf(vertex2)] = false;
+    		edges[vertices.indexOf(vertex2)][vertices.indexOf(vertex1)] = false;
+    	}
+    	return false;
         
     }
 
@@ -57,7 +88,12 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean isAdjacent(E vertex1, E vertex2) {
-        return false;
+        if(edges[vertices.indexOf(vertex1)][vertices.indexOf(vertex2)]
+        	&& edges[vertices.indexOf(vertex2)][vertices.indexOf(vertex1)]
+        	&& !vertex1.equals(vertex2))
+        	return true;
+    	
+    	return false;
         
     }
 
@@ -66,8 +102,17 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getNeighbors(E vertex) {
-        return null;
+        ArrayList<E> neighbors = new ArrayList<E>();
         
+        for(int col = 0; col < edges.length; col++)
+        {
+        	if(edges[vertices.indexOf(vertex)][col])
+        	{
+        		neighbors.add(vertices.get(vertices.indexOf(col)));
+        	}
+        	
+        }
+        return neighbors;
     }
 
     /**
@@ -75,8 +120,38 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getAllVertices() {
-        return null;
+        return (Iterable<E>) vertices;
         
     }
 
+}
+
+class Graphnode<T> {
+	private T data;
+	private boolean visited;
+//	private ArrayList<Graphnode<T>> edges;
+	
+	public Graphnode(T data)
+	{
+		this.data = data;
+		visited = false;
+//		edges = new ArrayList<Graphnode<T>>();
+	}
+	
+//	public void addEdge(Graphnode node)
+//	{
+//		edges.add(node);
+//		node.edges.add(this);
+//		
+//	}
+//	
+//	public void removeEdge(Graphnode node)
+//	{
+//		edges.remove(node);
+//		node.edges.remove(this);
+//	}
+	
+	
+	
+	
 }
