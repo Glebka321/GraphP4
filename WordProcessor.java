caa6993102e11b1918a6a5a5a5d9e37d52931f0b
Glebka321 <gt.wildrocket@gmail.com>
2018-04-07 21:06:26 -0500

    Update WordProcessor.java
    I think that's all whats needed for this class. Did not have a lot of time to put much thought into this, feel free to edit.

--- WordProcessor.java
+++ WordProcessor.java
@@ -1,6 +1,12 @@
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -9,76 +15,30 @@ import java.util.stream.Stream;
  * @author sapan (sapan@cs.wisc.edu)
  */
 public class WordProcessor {
-    
+
     /**
      * Gets a Stream of words from the filepath.
      * 
      * The Stream should only contain trimmed, non-empty and UPPERCASE words.
      * 
-     * @see <a href="http://www.oracle.com/technetwork/articles/java/ma14-java-se-8-streams-2177646.html">java8 stream blog</a>
+     * @see <a href=
+     *      "http://www.oracle.com/technetwork/articles/java/ma14-java-se-8-streams-2177646.html">java8
+     *      stream blog</a>
      * 
      * @param filepath file path to the dictionary file
      * @return Stream<String> stream of words read from the filepath
      * @throws IOException exception resulting from accessing the filepath
      */
     public static Stream<String> getWordStream(String filepath) throws IOException {
-        /**
-         * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html">java.nio.file.Files</a>
-         * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/nio/file/Paths.html">java.nio.file.Paths</a>
-         * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/nio/file/Path.html">java.nio.file.Path</a>
-         * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html">java.util.stream.Stream</a>
-         * 
-         * class Files has a method lines() which accepts an interface Path object and 
-         * produces a Stream<String> object via which one can read all the lines from a file as a Stream.
-         * 
-         * class Paths has a method get() which accepts one or more strings (filepath),  
-         * joins them if required and produces a interface Path object
-         * 
-         * Combining these two methods:
-         *     Files.lines(Paths.get(<string filepath>))
-         *     produces
-         *         a Stream of lines read from the filepath
-         * 
-         * Once this Stream of lines is available, you can use the powerful operations available for Stream objects to combine 
-         * multiple pre-processing operations of each line in a single statement.
-         * 
-         * Few of these features:
-         *      1. map( )      [changes a line to the result of the applied function. Mathematically, line = operation(line)]
-         *          -  trim all the lines
-         *          -  convert all the lines to UpperCase
-         *          -  example takes each of the lines one by one and apply the function toString on them as line.toString() 
-         *             and returns the Stream:
-         *                  streamOfLines = streamOfLines.map(String::toString) 
-         * 
-         *      2. filter( )   [keeps only lines which satisfy the provided condition]  
-         *          -  can be used to only keep non-empty lines and drop empty lines
-         *          -  example below removes all the lines from the Stream which do not equal the string "apple" 
-         *                 and returns the Stream:
-         *                  streamOfLines = streamOfLines.filter(x -> x != "apple");
-         *                   
-         *      3. collect( )  [collects all the lines into a java.util.List object]
-         *          -  can be used in the function which will invoke this method to convert Stream<String> of lines to List<String> of lines
-         *          -  example below collects all the elements of the Stream into a List and returns the List:
-         *              List<String> listOfLines = streamOfLines.collect(Collectors::toList); 
-         * 
-         * Note: since map and filter return the updated Stream objects, they can chained together as:
-         *      streamOfLines.map(...).filter(a -> ...).map(...) and so on
-         */
-        
-        return null;
+        Stream<String> stream = Files.lines(Paths.get(filepath));
+        return stream.filter(line -> !line.isEmpty()).map(String::toUpperCase).map(String::trim);
     }
-    
+
     /**
-     * Adjacency between word1 and word2 is defined by:
-     * if the difference between word1 and word2 is of
-     *  1 char replacement
-     *  1 char addition
-     *  1 char deletion
-     * then 
-     *  word1 and word2 are adjacent
-     * else
-     *  word1 and word2 are not adjacent
-     *  
+     * Adjacency between word1 and word2 is defined by: if the difference between word1 and word2 is
+     * of 1 char replacement 1 char addition 1 char deletion then word1 and word2 are adjacent else
+     * word1 and word2 are not adjacent
+     * 
      * Note: if word1 is equal to word2, they are not adjacent
      * 
      * @param word1 first word
@@ -86,7 +46,52 @@ public class WordProcessor {
      * @return true if word1 and word2 are adjacent else false
      */
     public static boolean isAdjacent(String word1, String word2) {
-        return false;   
+        int cnt = 0; // counts the number of differences between the two strings
+        if (word1.compareTo(word2) == 0)
+            return false;
+        if (Math.abs(word1.length() - word2.length()) > 1)
+            return false;
+        // if strings have the same length compare character by character, and see how many
+        // replacements there are
+        if (word1.length() - word2.length() == 0) {
+            for (int i = 0; i < word1.length(); i++) {
+                if (Character.toString(word1.charAt(i))
+                                .compareTo(Character.toString(word2.charAt(i))) != 0) {
+                    cnt++;
+                }
+            }
+        }
+        if (word1.length() - word2.length() == 1) {
+            int i = 0;
+            while (i < word2.length() && Character.toString(word1.charAt(i))
+                            .compareTo(Character.toString(word2.charAt(i))) == 0) {
+                i++;
+            }
+            cnt++;
+            for (int j = i; j < word2.length(); j++) {
+                if (Character.toString(word1.charAt(j + 1))
+                                .compareTo(Character.toString(word2.charAt(j))) != 0) {
+                    cnt++;
+                }
+            }
+        }
+        if (word1.length() - word2.length() == -1) {
+            int i = 0;
+            while (i < word1.length() && Character.toString(word1.charAt(i))
+                            .compareTo(Character.toString(word2.charAt(i))) == 0) {
+                i++;
+            }
+            cnt++;
+            for (int j = i; j < word1.length(); j++) {
+                if (Character.toString(word1.charAt(j))
+                                .compareTo(Character.toString(word2.charAt(j + 1))) != 0) {
+                    cnt++;
+                }
+            }
+        }
+        if (cnt == 1)
+            return true;
+        return false;
     }
-    
+
 }
