f896500f9cd5d6cdee4b6683f71ec7ec3e90a862
Glebka321 <gt.wildrocket@gmail.com>
2018-04-14 23:27:30 -0500

    Update GraphProcessorTest.java
    Added 3 more test methods

--- GraphProcessorTest.java
+++ GraphProcessorTest.java
@@ -1,5 +1,9 @@
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -30,11 +34,37 @@ public class GraphProcessorTest {
 
 	@Test
 	public final void Test1() throws Exception {
-		gp.populateGraph("file.txt");
-		gp.shortestPathPrecomputation();
-		for(String s : gp.getShortestPath("RAPINE", "PATTIES")) System.out.println(s);
-		System.out.println(gp.getShortestDistance("RAPINE", "PATTIES"));
-		for(String s : gp.getShortestPath("RAPINE", "GIBLETS")) System.out.println(s);
-		System.out.println(gp.getShortestDistance("RAPINE", "GIBLETS"));
+	      gp.populateGraph("file.txt");
+	      gp.shortestPathPrecomputation();
+	      for(String s : gp.getShortestPath("RAPINE", "PATTIES")) System.out.println(s);
+	      System.out.println(gp.getShortestDistance("RAPINE", "PATTIES"));
+	      for(String s : gp.getShortestPath("RAPINE", "GIBLETS")) System.out.println(s);
+	      System.out.println(gp.getShortestDistance("RAPINE", "GIBLETS"));
 	}
+	@Test 
+        public final void testPopulateGraph() throws IOException {
+              Integer count = 6;
+              gp.clearGraph();
+              assertEquals(gp.populateGraph("filegraph.txt"), count);
+        }
+    
+        @Test
+        public final void testGetShortestPath() throws IOException {
+              List<String> list = Stream.of("cat", "hat", "heat", "wheat").collect(Collectors.toList());
+              String word1 = "cat";
+              String word2 = "wheat";
+              gp.clearGraph();
+              gp.populateGraph("filegraph.txt");
+              assertEquals(list, gp.getShortestPath(word1, word2));
+        }
+    
+       @Test 
+       public final void testGetShortestDistance() throws IOException {
+              Integer distance = 3;
+              String word1 = "cat";
+              String word2 = "wheat";
+              gp.clearGraph();
+              gp.populateGraph("filegraph.txt");
+              assertEquals(gp.getShortestDistance(word1, word2), distance);
+       }
 }
